8b97837478e01eaba38046c57ad3cc09a22243f5
Amirshahi Alireza <amirshah@eslpc3.intranet.epfl.ch>
2022-08-15 10:58:35 +0200

    train the transformer model

--- src/bio_transformer_training.py
+++ src/bio_transformer_training.py
@@ -7,9 +7,10 @@ from utils.BioT import BioTransformer, Epilepsy60Dataset, ImbalancedDataSampler
 import torch
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader, TensorDataset
-from torch.nn import BCEWithLogitsLoss
-from torch.optim import Adam
+from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss
+from torch.optim import SGD
 from tqdm import tqdm
+import torch.nn.functional as F
 
 
 def set_labels(x):
@@ -19,81 +20,97 @@ def set_labels(x):
         x['labels'][start:end] = 1
 
 
-df = pd.read_csv('../input/Epilepsiae/epilepsiae_labels.csv')
-df['labels'] = df['length'].apply(lambda x: np.zeros(x//256 - 4, dtype=int))
-df['onsets']= df['onsets'].apply(lambda x:json.loads(x.replace('\n', ',')))
-df['offsets']= df['offsets'].apply(lambda x:json.loads(x.replace('\n', ',')))
-df.apply(set_labels, axis=1)
+def train():
+    df = pd.read_csv('../input/Epilepsiae_info/epilepsiae_labels.csv')
+    df['labels'] = df['length'].apply(lambda x: np.zeros(x//256 - 4, dtype=np.int))
+    df['onsets']= df['onsets'].apply(lambda x:json.loads(x.replace('\n', ',')))
+    df['offsets']= df['offsets'].apply(lambda x:json.loads(x.replace('\n', ',')))
+    df.apply(set_labels, axis=1)
 
-training_set = [x for x in df['file_name'].tolist() if not x.startswith('Patient_1_')]
-test_set = [x for x in df['file_name'].tolist() if not x in training_set]
-df_file_name = df.set_index('file_name')
-
-
-X = {'train': np.zeros((0, 144)), 'test': np.zeros((0, 144))}
-labels = {'train': np.zeros((0, 1)), 'test': np.zeros((0, 1))}
-valid_labels = {'train': np.zeros(0), 'test': np.zeros(0)}
-dataset = {'train': training_set, 'test': test_set}
-for mode in ['train', 'test']:
-    for t_file in dataset[mode]:
-        with open('../input/zc/{}_zc.pickle'.format(t_file), 'rb') as pickle_file:
-            data = pickle.load(pickle_file)
-            X[mode] = np.concatenate((X[mode], data), axis=0)
-        y = df_file_name.loc[t_file, 'labels']
-        labels[mode] = np.concatenate((labels[mode], np.expand_dims(y,axis=1)))
-        valid_index = np.arange(start=valid_labels[mode].shape[0] + 59, stop=valid_labels[mode].shape[0] + y.shape[0])
-        valid_labels[mode] = np.concatenate((valid_labels[mode], valid_index))
-print(X["train"].shape)
-mean_train = np.mean(X["train"])
-std_train = np.std(X["train"])
-X["train"] = (X["train"] - mean_train) / std_train
-X["test"] = (X["test"] - mean_train) / std_train
-print(valid_labels["train"].shape)
+    training_set = [x for x in df['file_name'].tolist() if not x.startswith('Patient_1_')]
+    test_set = [x for x in df['file_name'].tolist() if not x in training_set]
+    df_file_name = df.set_index('file_name')
 
 
-d_model = 144
-n_heads = 2
-d_hid = 4*d_model
-seq_len = 60
-n_layers = 2
-n_out = 1
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print('device : ', device)
-model = BioTransformer(d_model=d_model, n_heads=n_heads, d_hid=d_hid, seq_len=seq_len, n_layers=n_layers, n_out=n_out, device=device).to(device)
-
-#%%
-seizure_indices = np.where(labels['train']== 1)[0]
-non_seizure_indices = np.where(labels['train'] == 0)[0]
-valid_seizures = np.intersect1d(seizure_indices.astype(np.int32),valid_labels["train"].astype(np.int32))
-valid_non_seizure = np.intersect1d(non_seizure_indices.astype(np.int32),valid_labels["train"].astype(np.int32))
-
-X_train = X["train"]
-Y = labels["train"]
-print(Y.dtype)
-print(X_train.shape, Y.shape)
-train_set = Epilepsy60Dataset( torch.from_numpy(X_train).float(), torch.from_numpy(Y).float())
-sampler = ImbalancedDataSampler(torch.from_numpy(valid_seizures), torch.from_numpy(valid_non_seizure))
-train_loader = DataLoader(train_set, batch_size=16, sampler=sampler)
+    X = {'train': np.zeros((0, 144)), 'test': np.zeros((0, 144))}
+    labels = {'train': np.zeros((0, 1)), 'test': np.zeros((0, 1))}
+    valid_labels = {'train': np.zeros(0), 'test': np.zeros(0)}
+    dataset = {'train': training_set, 'test': test_set}
+    for mode in ['train', 'test']:
+        for t_file in dataset[mode]:
+            with open('../input/Epilepsiae_info/{}_zc.pickle'.format(t_file), 'rb') as pickle_file:
+                data = pickle.load(pickle_file)
+                X[mode] = np.concatenate((X[mode], data), axis=0)
+            y = df_file_name.loc[t_file, 'labels']
+            # y_onehot = np.zeros((y.size, 2))
+            # y_onehot[np.arange(y.size), y] = 1
+            labels[mode] = np.concatenate((labels[mode], np.expand_dims(y,axis=1)))
+            valid_index = np.arange(start=valid_labels[mode].shape[0] + 59, stop=valid_labels[mode].shape[0] + y.shape[0])
+            valid_labels[mode] = np.concatenate((valid_labels[mode], valid_index))
+    print(X["train"].shape)
+    mean_train = np.mean(X["train"])
+    std_train = np.std(X["train"])
+    X["train"] = (X["train"] - mean_train) / std_train
+    X["test"] = (X["test"] - mean_train) / std_train
+    print(valid_labels["train"].shape)
 
 
- # Training loop
-optimizer = Adam(model.parameters(), lr=0.01)
-criterion = BCEWithLogitsLoss()
-N_EPOCHS = 5
+    d_model = 144
+    n_heads = 2
+    d_hid = 4*d_model
+    seq_len = 60
+    n_layers = 8
+    n_out = 2
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('device : ', device)
+    model = BioTransformer(d_model=d_model, n_heads=n_heads, d_hid=d_hid, seq_len=seq_len, n_layers=n_layers, n_out=n_out, device=device).to(device)
 
-model.train()
-for epoch in tqdm(range(N_EPOCHS), desc="Training"):
-    train_loss = 0.0
-    for batch in tqdm(train_loader, desc=f"Epoch {epoch + 1}", leave=False):
-        x, y = batch['x'], batch['y']
-        x, y = x.to(device), y.to(device)
-        y_hat = model(x)
-        loss = criterion(y_hat[:,-1,:], y)
+    #%%
+    seizure_indices = np.where(labels['train']== 1)[0]
+    non_seizure_indices = np.where(labels['train'] == 0)[0]
+    valid_seizures = np.intersect1d(seizure_indices.astype(np.int32),valid_labels["train"].astype(np.int32))
+    valid_non_seizure = np.intersect1d(non_seizure_indices.astype(np.int32),valid_labels["train"].astype(np.int32))
 
-        train_loss += loss.detach().cpu().item()
+    X_train = X["train"]
+    Y = labels["train"]
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    print(Y.dtype)
+    print(X_train.shape, Y.shape)
+    train_set = Epilepsy60Dataset(torch.from_numpy(X_train).float(), torch.from_numpy(Y).long())
+    sampler = ImbalancedDataSampler(torch.from_numpy(valid_seizures), torch.from_numpy(valid_non_seizure))
+    train_loader = DataLoader(train_set, batch_size=16, sampler=sampler,  num_workers=6)
 
-    print(f"Epoch {epoch + 1}/{N_EPOCHS} loss: {train_loss:.2f}")
+
+    # Training loop
+    optimizer = SGD(model.parameters(), lr=5.0)
+    criterion = CrossEntropyLoss()
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.97)
+    N_EPOCHS = 250
+
+    model.train()  # turn on train mode
+    for epoch in tqdm(range(N_EPOCHS), desc="Training"):
+        train_loss = 0.0
+        for i, batch in enumerate(tqdm(train_loader, desc=f"Epoch {epoch + 1}",  position=0, leave=True)):
+            optimizer.zero_grad()
+            x, y = batch['x'], batch['y']
+            x, y = x.to(device), y.to(device)
+            y_hat = model(x)
+            loss = criterion(y_hat[:,-1,:], y.view(-1,))
+
+            train_loss += loss.detach().cpu().item()
+
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+            optimizer.step()
+        scheduler.step()
+        lr = scheduler.get_last_lr()[0]
+
+        print(f"Epoch {epoch + 1}/{N_EPOCHS} loss: {train_loss:.2f} lr {lr:.2f}")
+
+
+    torch.save(model, '../output/model60_n{}'.format(n_layers))
+    torch.save(model.state_dict(), '../output/model60_state_n{}'.format(n_layers))
+
+
+if __name__ == '__main__':
+    train()
--- src/utils/BioT.py
+++ src/utils/BioT.py
@@ -5,6 +5,7 @@ import numpy as np
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.sampler import Sampler
+import torch.nn.functional as F
 torch.manual_seed(1)
 
 
@@ -19,29 +20,49 @@ class BioTransformer(nn.Module):
 
         encoder_layers = TransformerEncoderLayer(d_model, n_heads, d_hid)
         self.transformer_encoder = TransformerEncoder(encoder_layers, n_layers)
+        self.pos_encoder = PositionalEncoding(d_model, dropout=0.1, max_len=seq_len)
         self.decoder = nn.Linear(d_model, n_out)
         self.sigmoid = nn.Sigmoid()
 
         self.class_token = nn.Parameter(torch.rand(1, d_model))
 
-    def get_positional_embeddings(self):
-        result = torch.ones(self.seq_len, self.d_model)
-        for i in range(self.seq_len):
-            for j in range(self.d_model):
-                result[i][j] = np.sin(i / (10000 ** (j / self.d_model))) if j % 2 == 0 else np.cos(i / (10000 ** ((j - 1) / self.d_model)))
-        return result
+        self.init_weights()
+
+    def init_weights(self) -> None:
+        initrange = 0.1
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
 
     def forward(self, features):
         n, c, h = features.shape
-        tokens = features
+        tokens = self.pos_encoder(features)
         # tokens = torch.stack([torch.vstack((self.class_token, tokens[i])) for i in range(len(tokens))])
-        tokens += self.get_positional_embeddings().repeat(n, 1, 1).to(self.device)
-
         output = self.transformer_encoder(tokens)
         output = self.decoder(output)
         # output = self.sigmoid(output)
         return output
 
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(1, max_len, d_model)
+        pe[0, :, 0::2] = torch.sin(position * div_term)
+        pe[0, :, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Args:
+            x: Tensor, shape [batch_size, seq_len, embedding_dim]
+        """
+        x = x + self.pe
+        return self.dropout(x)
 
 
 class Epilepsy60Dataset(Dataset):
@@ -56,10 +77,7 @@ class Epilepsy60Dataset(Dataset):
         # if torch.is_tensor(idx):
         #     idx = idx.tolist()
 
-        if idx < 59:
-            x60 = torch.zeros((60, 144))
-        else:
-            x60 = self.x_total[idx-59:idx+1, :]
+        x60 = self.x_total[idx-59:idx+1, :]
 
         sample = {'x': x60, 'y': self.y_total[idx]}
         return sample
@@ -74,7 +92,8 @@ class ImbalancedDataSampler(Sampler):
 
     def __iter__(self):
         sampled_non_seizure_indices = torch.randperm(self.num_non_seizure)[:self.num_seizure]
-        return iter(torch.cat((self.seizure_indices, self.non_seizure_indices[sampled_non_seizure_indices]), 0))
+        seizure_non_seizure_cat = torch.cat((self.seizure_indices, self.non_seizure_indices[sampled_non_seizure_indices]), 0)
+        return iter(seizure_non_seizure_cat[torch.randperm(2*self.num_seizure)])
 
     def __len__(self):
         return 2*self.num_seizure
