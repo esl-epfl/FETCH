a135eea2d9637953ad81de84938e58a7f5f42550
Amirshahi Alireza <amirshah@eslpc3.intranet.epfl.ch>
2022-12-31 11:22:46 +0100

    iterate and anootate all the files

--- src/utils/edf_labeling.py
+++ src/utils/edf_labeling.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from utils.params import TUSZ_BENDR_channels, BENDR_channels
 import json
+import os
 
 
 def get_annotation(filename, df):
@@ -15,8 +16,6 @@ def get_annotation(filename, df):
 
     '''
     row = df.loc[filename.split('/')[-1]]
-    print(row)
-    print(row['onsets'])
     fs = row['fs_FP1']
     onsets = [float(int(x[0]))/fs for x in row['onsets']]
     offsets = [float(int(x[0]))/fs for x in row['offsets']]
@@ -37,11 +36,11 @@ def get_annotation(filename, df):
         annotation.append((onsets[-1], offsets[-1], 'T1'))
         if offsets[-1] != length:
             annotation.append((offsets[-1],length, 'T0'))
-        print(annotation)
         return annotation
 
 
-def convert_signal(filename, df):
+def convert_signal(filename, df, save_path=None):
+    global min_dataset
     f = pyedflib.EdfReader(filename + ".edf")
     signal_labels = f.getSignalLabels()
     valid_channels = []
@@ -51,7 +50,9 @@ def convert_signal(filename, df):
     num_channels = len(valid_channels)
     assert num_channels != 0, "Number of valid channels is zero!"
 
-    f_w = pyedflib.EdfWriter(filename + "_annotated.edf", num_channels,
+    if save_path is None:
+        save_path = filename
+    f_w = pyedflib.EdfWriter(save_path + "_annotated.edf", num_channels,
                              file_type=pyedflib.FILETYPE_EDFPLUS)
     data_list = []
     for write_ch, ch in enumerate(valid_channels):
@@ -67,12 +68,11 @@ def convert_signal(filename, df):
     annotation = get_annotation(filename, df)
     for annot in annotation:
         f_w.writeAnnotation(annot[0], annot[1], annot[2])
-    print('min', np.min(data_list))
-    print('max', np.max(data_list))
     f_w.writeSamples(data_list)
 
     f.close()
     f_w.close()
+    return np.min(data_list) ,  np.max(data_list)
 
 
 def read_signal(filename):
@@ -87,16 +87,37 @@ def read_signal(filename):
     f.close()
 
 
+def get_full_path(x):
+    mode, tcp, pat_group, pat_s_t = x.split('/')
+    pat, s, _ = pat_s_t.split('_')
+
+    s_fullname = [filename for filename in os.listdir("/home/amirshah/EPFL/BENDR/data/TUSZ/edf/{}/{}/{}/{}/".format(mode, tcp, pat_group, pat)) if
+     filename.startswith(s)]
+    assert len(s_fullname) == 1, "Error {}".format(s_fullname)
+    return "{}/{}/{}/{}/{}/{}".format(mode, tcp, pat_group, pat, s_fullname[0], pat_s_t)
+
+
 if __name__ == '__main__':
-    # convert_signal("../../input/chb-mit-bendr/s005/chb02_30.edf")
-    # read_signal("../../input/chb-mit-bendr/s005/chb02_30_new.edf")
     df = pd.read_csv('../../input/TUSZ/TUSZ_labels.csv')
     df['onsets'] = df['onsets'].apply(lambda x: json.loads(x.replace('\n', ',')))
     df['offsets'] = df['offsets'].apply(lambda x: json.loads(x.replace('\n', ',')))
     df = df.set_index('file_name_edf')
-    print(df[df['onsets'].apply(lambda x: len(x) > 1)]['file_name'])
-    for i in [4, 7, 9, 10, 16]:
-        file = "/home/amirshah/EPFL/BENDR/data/TUSZ/edf/train/03_tcp_ar_a/130/00013085/s002_2015_10_05/00013085_s002_t0{:02d}".format(i)
-        print(file)
-        convert_signal(file, df)
-    # read_signal(file + "_annotated.edf")
+    pd.set_option('max_columns', None)
+    print(df[df['offsets'].apply(lambda x: x[0][0] == 0)].sample(n=5))
+    minimum = 0
+    maximum = 0
+
+    df['full_path'] = df['file_name'].apply(get_full_path)
+    seizure_df = df[df['offsets'].apply(lambda x: x[0][0] == 0)]
+    for mode in ['trainSet', 'devSet', 'testSet']:
+        seizure_files = seizure_df[seizure_df['mode'] == mode]['full_path'].tolist()
+        os.mkdir(os.path.join('../../input/TUSZ', mode))
+        for sf in seizure_files:
+            save_path = '../../input/TUSZ/{}/{}'.format(mode, sf.split('/')[-1])
+            min_dataset , max_dataset = convert_signal("/home/amirshah/EPFL/BENDR/data/TUSZ/edf/"+sf.split('.edf')[0],
+                                                       df, save_path=save_path)
+            if mode == 'trainSet':
+                minimum = min(min_dataset, minimum)
+                maximum = max(max_dataset, maximum)
+    print("Min Max: ", minimum, maximum)
+
