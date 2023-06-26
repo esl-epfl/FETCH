a5248d563515ac9a35462bf60457e4056426bd69
Amirshahi Alireza <amirshah@eslpc3.intranet.epfl.ch>
2023-06-26 10:13:34 +0200

    apply seizure detection on TUSZ_v2 with true split

--- TSD/code/inference.py
+++ TSD/code/inference.py
@@ -18,6 +18,11 @@ from scipy.signal import stft
 from tqdm import tqdm
 from einops import rearrange, repeat
 from einops.layers.torch import Rearrange
+from tuh_dataset import TUHDataset
+from epilepsy_performance_metrics.src.timescoring.annotations import Annotation
+from epilepsy_performance_metrics.src.timescoring.annotations import Annotation
+import torch.multiprocessing
+torch.multiprocessing.set_sharing_strategy('file_system')
 
 
 def seed_everything(seed=99):
@@ -31,188 +36,6 @@ def seed_everything(seed=99):
 
 
 seed_everything(seed=55)
-
-
-class PreNorm(nn.Module):
-    def __init__(self, dim, fn):
-        super().__init__()
-        self.norm = nn.LayerNorm(dim)
-        self.fn = fn
-
-    def forward(self, x, **kwargs):
-        return self.fn(self.norm(x), **kwargs)
-
-
-class FeedForward(nn.Module):
-    def __init__(self, dim, hidden_dim, dropout=0.):
-        super().__init__()
-        self.net = nn.Sequential(nn.Linear(dim, hidden_dim), nn.GELU(), nn.Dropout(dropout), nn.Linear(hidden_dim, dim),
-                                 nn.Dropout(dropout))
-
-    def forward(self, x):
-        return self.net(x)
-
-
-class Attention(nn.Module):
-    def __init__(self, dim, heads=8, dim_head=64, dropout=0.):
-        super().__init__()
-        inner_dim = dim_head * heads
-        project_out = not (heads == 1 and dim_head == dim)
-
-        self.heads = heads
-        self.scale = dim_head ** -0.5
-
-        self.attend = nn.Softmax(dim=-1)
-        self.dropout = nn.Dropout(dropout)
-
-        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias=False)
-
-        self.to_out = nn.Sequential(nn.Linear(inner_dim, dim), nn.Dropout(dropout)) if project_out else nn.Identity()
-
-    def forward(self, x):
-        qkv = self.to_qkv(x).chunk(3, dim=-1)
-        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=self.heads), qkv)
-
-        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
-
-        attn = self.attend(dots)
-        attn = self.dropout(attn)
-
-        out = torch.matmul(attn, v)
-        out = rearrange(out, 'b h n d -> b n (h d)')
-        return self.to_out(out)
-
-
-class Transformer(nn.Module):
-    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout=0.):
-        super().__init__()
-        self.layers = nn.ModuleList([])
-        for _ in range(depth):
-            self.layers.append(nn.ModuleList(
-                [PreNorm(dim, Attention(dim, heads=heads, dim_head=dim_head, dropout=dropout)),
-                 PreNorm(dim, FeedForward(dim, mlp_dim, dropout=dropout))]))
-
-    def forward(self, x):
-        for attn, ff in self.layers:
-            x = attn(x) + x
-            x = ff(x) + x
-        return x
-
-
-class EEGVT(nn.Module):
-    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool='cls', channels=3,
-                 dim_head=64, dropout=0., emb_dropout=0.):
-        super().__init__()
-        image_height, image_width = image_size if isinstance(image_size, tuple) else (image_size, image_size)
-        patch_height, patch_width = patch_size if isinstance(patch_size, tuple) else (patch_size, patch_size)
-
-        assert image_height % patch_height == 0 and image_width % patch_width == 0
-
-        num_patches = (image_height // patch_height) * (image_width // patch_width)
-        patch_dim = channels * patch_height * patch_width
-        assert pool in {'cls', 'mean'}
-
-        self.to_patch_embedding = nn.Sequential(
-            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=patch_height, p2=patch_width),
-            nn.Linear(patch_dim, dim), )
-
-        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
-        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
-        self.dropout = nn.Dropout(emb_dropout)
-
-        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
-
-        self.pool = pool
-        self.to_latent = nn.Identity()
-
-        self.mlp_head = nn.Sequential(nn.LayerNorm(dim), nn.Linear(dim, num_classes))
-
-    def forward(self, img):
-        x = self.to_patch_embedding(img)
-        b, n, _ = x.shape
-
-        cls_tokens = repeat(self.cls_token, '1 1 d -> b 1 d', b=b)
-        x = torch.cat((cls_tokens, x), dim=1)
-        x += self.pos_embedding[:, :(n + 1)]
-        x = self.dropout(x)
-
-        x = self.transformer(x)
-
-        x = x.mean(dim=1) if self.pool == 'mean' else x[:, 0]
-
-        x = self.to_latent(x)
-        return self.mlp_head(x)
-
-
-def bipolar_signals_func(signals):
-    bipolar_signals = []
-    bipolar_signals.append(signals[0] - signals[4])  # fp1-f7
-    bipolar_signals.append(signals[1] - signals[5])  # fp2-f8
-    bipolar_signals.append(signals[4] - signals[9])  # f7-t3
-    bipolar_signals.append(signals[5] - signals[10])  # f8-t4
-    bipolar_signals.append(signals[9] - signals[15])  # t3-t5
-    bipolar_signals.append(signals[10] - signals[16])  # t4-t6
-    bipolar_signals.append(signals[15] - signals[13])  # t5-o1
-    bipolar_signals.append(signals[16] - signals[14])  # t6-o2
-    bipolar_signals.append(signals[9] - signals[6])  # t3-c3
-    bipolar_signals.append(signals[7] - signals[10])  # c4-t4
-    bipolar_signals.append(signals[6] - signals[8])  # c3-cz
-    bipolar_signals.append(signals[8] - signals[7])  # cz-c4
-    bipolar_signals.append(signals[0] - signals[2])  # fp1-f3
-    bipolar_signals.append(signals[1] - signals[3])  # fp2-f4
-    bipolar_signals.append(signals[2] - signals[6])  # f3-c3
-    bipolar_signals.append(signals[3] - signals[7])  # f4-c4
-    bipolar_signals.append(signals[6] - signals[11])  # c3-p3
-    bipolar_signals.append(signals[7] - signals[12])  # c4-p4
-    bipolar_signals.append(signals[11] - signals[13])  # p3-o1
-    bipolar_signals.append(signals[12] - signals[14])  # p4-o2
-
-    return bipolar_signals
-
-
-def spectrogram_unfold_feature(signals):
-    nperseg = 250
-    noverlap = 50
-    freq_resolution = 2
-    nfft = sample_rate * freq_resolution
-    freqs, times, spec = stft(signals, fs=sample_rate, nperseg=nperseg, noverlap=noverlap, nfft=nfft, boundary=None,
-                              padded=False)
-
-    spec = spec[:, :spec.shape[1] - 1, :]
-    spec = np.reshape(spec, (-1, spec.shape[2]))
-    amp = (np.log(np.abs(spec) + 1e-10)).astype(np.float32)
-
-    return freqs, times, amp
-
-
-class TUHDataset(Dataset):
-    def __init__(self, file_list, transform=None):
-        self.file_list = file_list
-        self.file_length = len(self.file_list)
-        self.transform = transform
-
-    def __len__(self):
-        return self.file_length
-
-    def __getitem__(self, idx):
-        with open(self.file_list[idx], 'rb') as f:
-            data_pkl = pickle.load(f)
-
-            signals = np.asarray(bipolar_signals_func(data_pkl['signals']))
-            # print(signals.shape)
-
-            if eeg_type == 'stft':
-                f, t, signals = spectrogram_unfold_feature(signals)  # print(signals.shape)  # exit()
-
-            signals = self.transform(signals)
-            label = data_pkl['label']
-            label = 0. if label == "bckg" else 1.
-
-            patient_id = data_pkl['patient id']
-            # bipolar_channel_name = data_pkl['bipolar_channel_name']
-            confidence = data_pkl['confidence']
-
-        return signals, label, data_pkl['label']
 
 
 def get_data_loader(save_directory, batch_size=1):
@@ -237,17 +60,11 @@ def get_data_loader(save_directory, batch_size=1):
     shuffle(train_data)
     print('len(train_data): {}'.format(len(train_data)))
 
-    bckg_data = file_lists['val']['bckg'] + file_lists['test']['bckg']
-    shuffle(bckg_data)
-
-    seiz_data = file_lists['val']['seiz'] + file_lists['test']['seiz']
-    shuffle(seiz_data)
-
-    val_data = bckg_data[:int(len(bckg_data) / 2)] + seiz_data[:int(len(seiz_data) / 2)]
+    val_data = file_lists['val']['bckg'] + file_lists['val']['seiz']
     shuffle(val_data)
     print('len(val_data): {}'.format(len(val_data)))
 
-    test_data = bckg_data[int(len(bckg_data) / 2):] + seiz_data[int(len(seiz_data) / 2):]
+    test_data = file_lists['test']['bckg'] + file_lists['test']['seiz']
     shuffle(test_data)
     print('len(test_data): {}'.format(len(test_data)))
 
@@ -261,9 +78,9 @@ def get_data_loader(save_directory, batch_size=1):
     val_data = TUHDataset(val_data, transform=val_transforms)
     test_data = TUHDataset(test_data, transform=test_transforms)
 
-    train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
-    val_loader = DataLoader(dataset=val_data, batch_size=batch_size, shuffle=False)
-    test_loader = DataLoader(dataset=test_data, batch_size=batch_size, shuffle=False)
+    train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True, num_workers=8)
+    val_loader = DataLoader(dataset=val_data, batch_size=batch_size, shuffle=False, num_workers=8)
+    test_loader = DataLoader(dataset=test_data, batch_size=batch_size, shuffle=False, num_workers=8)
 
     return train_loader, val_loader, test_loader
 
@@ -278,7 +95,7 @@ def test():
 
     time_consumes = []
     with torch.no_grad():
-        for data, label, label_name in tqdm(test_loader):
+        for data, label in tqdm(test_loader):
             test_label_all.extend(label)
 
             start_time = time.perf_counter()
@@ -288,22 +105,27 @@ def test():
             test_prob = torch.squeeze(sigmoid(test_prob))
             test_prob_all = np.concatenate((test_prob_all, test_prob.cpu().numpy()))
 
+            # annotation_labels = Annotation(label.cpu().numpy(), 1/12)
+            # annotation_pred = Annotation(test_prob.cpu().numpy(), 1/12)
+            # scores =
+
     test_auc = roc_auc_score(test_label_all, test_prob_all)
     print(f"test_auc: {test_auc}")
     print('average_time: {}'.format(sum(time_consumes) / len(time_consumes)))
 
-    #############找阈值
-    def Find_Optimal_Cutoff(FPR, TPR, thresholds):
-        y = TPR - FPR
-        Youden_index = np.argmax(y)  # Only the first occurrence is returned.
-        optimal_threshold = thresholds[Youden_index]
-        point = [FPR[Youden_index], TPR[Youden_index]]
-        return optimal_threshold, point
 
-    fpr, tpr, thresholds = metrics.roc_curve(test_label_all, test_prob_all)
-    print(Find_Optimal_Cutoff(fpr, tpr, thresholds))
 
-    # #############画图部分  # roc_auc = metrics.auc(fpr, tpr)  # plt.figure(figsize=(6,6))  # plt.title('Validation ROC')  # plt.plot(fpr, tpr, 'b', label = 'Val AUC = %0.4f' % roc_auc)  # plt.legend(loc = 'lower right')  # plt.plot([0, 1], [0, 1],'r--')  # plt.xlim([0, 1])  # plt.ylim([0, 1])  # plt.ylabel('True Positive Rate')  # plt.xlabel('False Positive Rate')  # plt.show()
+    # def Find_Optimal_Cutoff(FPR, TPR, thresholds):
+    #     y = TPR - FPR
+    #     Youden_index = np.argmax(y)  # Only the first occurrence is returned.
+    #     optimal_threshold = thresholds[Youden_index]
+    #     point = [FPR[Youden_index], TPR[Youden_index]]
+    #     return optimal_threshold, point
+    #
+    # fpr, tpr, thresholds = metrics.roc_curve(test_label_all, test_prob_all)
+    # print(Find_Optimal_Cutoff(fpr, tpr, thresholds))
+
+  # roc_auc = metrics.auc(fpr, tpr)  # plt.figure(figsize=(6,6))  # plt.title('Validation ROC')  # plt.plot(fpr, tpr, 'b', label = 'Val AUC = %0.4f' % roc_auc)  # plt.legend(loc = 'lower right')  # plt.plot([0, 1], [0, 1],'r--')  # plt.xlim([0, 1])  # plt.ylim([0, 1])  # plt.ylabel('True Positive Rate')  # plt.xlabel('False Positive Rate')  # plt.show()
 
 
 def get_data_loader_multi(save_directory, batch_size=1):
@@ -397,14 +219,14 @@ def test_run():
             # print(test_prob.cpu().numpy())
 
 
-sample_rate = 250
+sample_rate = 256
 eeg_type = 'stft'  # 'original', 'bipolar', 'stft'
 device = 'cuda:0'
 # device = 'cpu'
 
-model = torch.load('inference_ck_0.9208', map_location=torch.device(device))
-# model = torch.load('/home/amirshah/EPFL/EpilepsyTransformer/TUSZv2/preprocess/test3/test_model_0.867288059115128',
-#                    map_location=torch.device(device))
+# model = torch.load('inference_ck_0.9208', map_location=torch.device(device))
+model = torch.load('/home/amirshah/EPFL/EpilepsyTransformer/TUSZv2/preprocess/test_v2/test_model_0.9374276011157442',
+                   map_location=torch.device(device))
 model.eval()
 sigmoid = nn.Sigmoid()
 
